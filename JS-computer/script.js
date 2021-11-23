1ba187a7dadc4fdcaecc8ca9c2d9f07685b1e3f4
JordiPV <88150345+JorPV@users.noreply.github.com>
2021-11-23 14:44:46 +0100

    Add files via upload

---
+++ JS-computer/script.js
@@ -0,0 +1,9 @@
+console.log('Hello Winc Academy Students');
+// This is a large sum
+/* let multiply = 1230941 * 1823794; 
+console.log(multiply); 
+let divide = 637263 / 54;
+console.log(divide); */
+// I do not see the comments because JavaScript doesn't read comments
+
+
